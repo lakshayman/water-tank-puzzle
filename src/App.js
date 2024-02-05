63aa4c27da85e0411e9f32cd91c8f7c5567a03cf
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-02-05 16:15:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Tank } from "./Components/Tank";
 
 function App() {
+  const [noOfTanks, setNoOfTanks] = useState(0);
+  const [quantityInTanks, setQuantityInTanks] = useState(
+    Array(noOfTanks).fill(0)
+  );
+  const [quantityInMagicTanks, setQuantityInMagicTanks] = useState(
+    Array(noOfTanks).fill(0)
+  );
+
+  useEffect(() => {
+    setQuantityInTanks(Array(noOfTanks).fill(0));
+    setQuantityInMagicTanks(Array(noOfTanks).fill(0));
+  }, [noOfTanks]);
+
+  useEffect(() => {
+    const ifArrayIsWithEqualValues = (arr, value) => {
+      console.log(arr);
+      for(let i = 0; i < arr.length; i++) {
+        if (arr[i] !== value) {
+          return false;
+        }
+      }
+      return true;
+    };
+    let timer1 = setInterval(() => {
+      console.log("Running");
+      setQuantityInMagicTanks((prev1) => {
+        console.log(!ifArrayIsWithEqualValues(prev1, 0));
+        if (!ifArrayIsWithEqualValues(prev1, 0)) {
+          const totalInMagicTank = prev1.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+          );
+          const flow = Math.min(25, totalInMagicTank);
+          const avgQty = flow / prev1.length;
+          setQuantityInTanks((prev) => {
+            const waterInTanks = prev.map((val) => val + Math.min(avgQty, 1000 - val));
+            return waterInTanks;
+          });
+          return prev1.map((val) => {
+            return val - ((val / totalInMagicTank) * flow);
+          });
+        }
+        return prev1;
+      });
+      setQuantityInTanks((prev) => {
+        const waterInTanks = [...prev];
+        if(!ifArrayIsWithEqualValues(waterInTanks, waterInTanks[0])) {
+          const totalWaterInTanks = waterInTanks.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+          );
+          const avgQtyInTank = totalWaterInTanks/waterInTanks.length;
+          let tanksWithMoreWater = 0, tanksWitLessWater = 0;
+          for(let i = 0; i < waterInTanks.length; i++) {
+            if(waterInTanks[i] > avgQtyInTank) {
+              tanksWithMoreWater++;
+            } else if(waterInTanks[i] < avgQtyInTank) {
+              tanksWitLessWater++;
+            }
+          }
+          return waterInTanks.map(water => {
+            if(water > avgQtyInTank) {
+              return Math.max(avgQtyInTank, water - (25/tanksWithMoreWater));
+            } else if(water < avgQtyInTank) {
+              return Math.min(avgQtyInTank, water + (25/tanksWitLessWater));
+            } else {
+              return water;
+            }
+          })
+        }
+        return waterInTanks;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(timer1);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-heading">Water Tank Puzzle</h1>
+      <button
+        onClick={() => {
+          setNoOfTanks((prev) => prev + 1);
+        }}
+      >
+        Add Tank
+      </button>
+      <div className="tank-container">
+        {quantityInTanks.map((quantity, index) => {
+          return (
+            <Tank
+              noOfTanks={noOfTanks}
+              quantity={quantity}
+              quantityInMagicTank={quantityInMagicTanks[index]}
+              index={index}
+              setQuantityInTanks={setQuantityInTanks}
+              setQuantityInMagicTanks={setQuantityInMagicTanks}
+            />
+          );
+        })}
+      </div>
+      <h1 className="App-heading">
+        {noOfTanks} {quantityInTanks}
+      </h1>
     </div>
   );
 }
---
+++ src/Components/Tank.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export const Tank = ({
+  noOfTanks,
+  quantity,
+  index,
+  setQuantityInTanks,
+  quantityInMagicTank,
+  setQuantityInMagicTanks,
+}) => {
+  const [timer, setTimer] = useState(null);
+  const [buttonName, setButtonName] = useState("Add Water");
+
+  return (
+    <div className="tank-box">
+      <div
+        className="tank"
+        style={{
+          background: `linear-gradient(to bottom, transparent 0%,transparent ${
+            100 - (quantity / 10 + quantityInMagicTank / 10)
+          }%, green ${
+            100 - (quantity / 10 + quantityInMagicTank / 10)
+          }%,green ${100 - quantity / 10}%,#84cdee ${
+            100 - quantity / 10
+          }%,#84cdee 100%)`,
+        }}
+      ></div>
+      <button
+        onMouseDown={() => {
+          setButtonName("Filling Water");
+          const timer = setInterval(() => {
+            setQuantityInMagicTanks((prev) => {
+              const newQuantityInMagicTanks = [...prev];
+              newQuantityInMagicTanks[index] =
+                1000 - (newQuantityInMagicTanks[index] + quantity) > 200
+                  ? newQuantityInMagicTanks[index] + 200
+                  : newQuantityInMagicTanks[index] +
+                    (1000 - (newQuantityInMagicTanks[index] + quantity));
+              return newQuantityInMagicTanks;
+            });
+          }, 1000);
+          setTimer(timer);
+        }}
+        onMouseUp={() => {
+          setButtonName("Add Water");
+          clearInterval(timer);
+        }}
+      >
+        {buttonName}
+      </button>
+      <button
+        onClick={() => {
+          setQuantityInMagicTanks((prev) => {
+            const newQuantityInTanks = [...prev];
+            newQuantityInTanks[index] = 0;
+            return newQuantityInTanks;
+          });
+          setQuantityInTanks((prev) => {
+            const newQuantityInTanks = [...prev];
+            newQuantityInTanks[index] = 0;
+            return newQuantityInTanks;
+          });
+        }}
+      >
+        Empty Tank
+      </button>
+    </div>
+  );
+};
